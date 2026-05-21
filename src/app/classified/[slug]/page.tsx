857f1de52e4e0b25ba0c6c9341ee85336796afc8
root <root@srv1489383.hstgr.cloud>
2026-05-21 06:43:05 +0000

    Add singular classified detail route alias

--- src/app/classified/[slug]/page.tsx
+++ src/app/classified/[slug]/page.tsx
@@ -1,26 +1,4 @@
-import { notFound } from "next/navigation";
-import { TaskDetailPage } from "@/components/tasks/task-detail-page";
-import { fetchTaskPostBySlug } from "@/lib/task-data";
-import { buildPostMetadata } from "@/lib/seo";
+import ClassifiedDetailPage, { generateMetadata, generateStaticParams, revalidate } from "../../classifieds/[slug]/page";
 
-export const revalidate = 3;
-export const dynamic = "force-dynamic";
-
-type PageParams = { params: Promise<{ slug: string }> | { slug: string } };
-
-const resolveSlug = async (params: PageParams["params"]) =>
-  (await Promise.resolve(params)).slug;
-
-export async function generateMetadata({ params }: PageParams) {
-  const slug = await resolveSlug(params);
-  const post = await fetchTaskPostBySlug("classified", slug);
-  if (!post) return {};
-  return buildPostMetadata("classified", post);
-}
-
-export default async function ClassifiedDetailPage({ params }: PageParams) {
-  const slug = await resolveSlug(params);
-  const post = await fetchTaskPostBySlug("classified", slug);
-  if (!post) notFound();
-  return <TaskDetailPage task="classified" slug={slug} />;
-}
+export { generateMetadata, generateStaticParams, revalidate };
+export default ClassifiedDetailPage;
